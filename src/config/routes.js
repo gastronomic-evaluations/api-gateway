1f52b614f376411923048c14b697a7c0e2c5020a
Jefferson Daniel <jeffersondanielss@gmail.com>
2019-07-24 15:38:31 -0300

    feat: users api route

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,17 +3,20 @@ const httpProxy = require('express-http-proxy');
 const {
   GASTRONOMIC_EVALUATIONS_API,
   GASTRONOMIC_EVALUATIONS_APP,
-  LANDING_PAGE
+  LANDING_PAGE,
+  GE_USERS_API
 } = process.env
 
 const ApiServiceProxy = httpProxy(GASTRONOMIC_EVALUATIONS_API);
 const AppServiceProxy = httpProxy(GASTRONOMIC_EVALUATIONS_APP);
+const UserServiceProxy = httpProxy(GE_USERS_API);
 const LandingPageServiceProxy = httpProxy(LANDING_PAGE);
 
 module.exports = (app) => {
   app.route(['/', '/assets/*']).get(LandingPageServiceProxy);
   app.route('/api/*').all(ApiServiceProxy);
   app.route('/app/*').get(AppServiceProxy);
+  app.route('/users/api/*').get(UserServiceProxy);
   app.route('/healthcheck')
     .get((req, res) => res.status(200).json({ status: 'ok' }));
 }
